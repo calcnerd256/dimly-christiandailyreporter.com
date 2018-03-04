c6cad22ea472b42bacb60e978672b7bd3ef84ff6
Dimly spider <calcnerd256+dimly@gmail.com>
2018-03-04 08:12:13 -0600

    exit status and delete absent files

--- step.sh
+++ step.sh
@@ -3,6 +3,7 @@
 cd "$(dirname "$(readlink -f "$0")")"
 
 rm -r www
-wget -xHkKpP www https://www.christiandailyreporter.com/
-git add www
+wget -xHkKpP www https://www.christiandailyreporter.com/ || exit 1
+git add -A www
 git commit -m "fetched at $(date)"
+exit 0
