a078f88df81ce5f7537e5360106bdd12a75f43d2
Dimly spider <calcnerd256+dimly@gmail.com>
2018-03-04 09:01:49 -0600

    User-Agent header and a variable for destination directory

--- step.sh
+++ step.sh
@@ -2,8 +2,11 @@
 
 cd "$(dirname "$(readlink -f "$0")")"
 
-rm -r www
-wget -xHkKpP www https://www.christiandailyreporter.com/ || exit 1
-git add -A www
+USER_AGENT="a wget spider which is part of project dimly and can be found at https://github.com/calcnerd256/dimly-christiandailyreporter.com"
+WWW_DIR="www"
+
+rm -r "$WWW_DIR"
+wget -xHkKpP "$WWW_DIR" -U "$USER_AGENT" https://www.christiandailyreporter.com/ || exit 1
+git add -A "$WWW_DIR"
 git commit -m "fetched at $(date)" && git push github master
 exit 0
